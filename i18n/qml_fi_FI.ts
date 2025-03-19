c23ad0ea6f0e5217b225314aa1d95b4d4453bedb
teskola <teskola@proton.me>
2025-03-19 21:49:55 +0200

    CMake settings

---
+++ i18n/qml_fi_FI.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi_FI">
+<context>
+    <name>main</name>
+    <message>
+        <source>Speedtest</source>
+        <translation>NopeusTesti</translation>
+    </message>
+    <message>
+        <source>Points: </source>
+        <translation>Pisteet: </translation>
+    </message>
+    <message>
+        <source>New record!</source>
+        <translation>Uusi ennätys!</translation>
+    </message>
+    <message>
+        <source>New game</source>
+        <translation>Uusi peli</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Lopeta</translation>
+    </message>
+</context>
+</TS>
